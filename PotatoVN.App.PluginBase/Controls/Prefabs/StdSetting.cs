4f277b61c7b4ce2518b7c1e6e5e60b0e16efc63a
goldenpotato137 <goldenpotato137@gmail.com>
2025-10-01 23:40:40 +0800

    feat: 添加一些标准控件

---
+++ PotatoVN.App.PluginBase/Controls/Prefabs/StdSetting.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace PotatoVN.App.PluginBase.Controls.Prefabs
+{
+    public sealed class Setting : UserControl
+    {
+        public Setting(string title, string description, FrameworkElement rightContent)
+        {
+            rightContent.HorizontalAlignment = HorizontalAlignment.Right;
+
+            var titleText = new TextBlock { Text = title };
+            var descriptionText = new TextBlock
+            {
+                Text = description,
+                TextWrapping = TextWrapping.Wrap
+            };
+            TryApplyStyle(titleText, "BodyTextBlockStyle");
+            TryApplyStyle(descriptionText, "DescriptionTextStyle");
+
+            var leftStack = new StackPanel { Orientation = Orientation.Vertical };
+            leftStack.Children.Add(titleText);
+            leftStack.Children.Add(descriptionText);
+
+            var root = new Grid();
+            root.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(8, GridUnitType.Star) });
+            root.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
+            root.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+            
+            //左侧
+            root.Children.Add(leftStack);
+            Grid.SetColumn(leftStack, 0);
+            //中间空隙
+            var spacer = new Grid();
+            root.Children.Add(spacer);
+            Grid.SetColumn(spacer, 1);
+            //右侧
+            root.Children.Add(rightContent);
+            Grid.SetColumn(rightContent, 2);
+
+            Content = root;
+        }
+        
+        private static void TryApplyStyle(FrameworkElement fe, string key)
+        {
+            if (Application.Current?.Resources != null &&
+                Application.Current.Resources.TryGetValue(key, out var v) &&
+                v is Style s)
+            {
+                fe.Style = s;
+            }
+        }
+    }
+}
--- PotatoVN.App.PluginBase/Plugin_Ui.cs
+++ PotatoVN.App.PluginBase/Plugin_Ui.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using PotatoVN.App.PluginBase.Controls;
+using PotatoVN.App.PluginBase.Controls.Prefabs;
+using PotatoVN.App.PluginBase.Helper;
 
 namespace PotatoVN.App.PluginBase;
 
@@ -6,6 +10,9 @@ public partial class Plugin
 {
     public FrameworkElement CreateSettingUi()
     {
-        return new Controls.UserControl1();
+        StdStackPanel panel = new();
+        panel.Children.Add(new UserControl1().WarpWithPanel());
+        panel.Children.Add(new Setting("设置标题", "这是一个设置", new ToggleSwitch()).WarpWithPanel());
+        return panel;
     }
 }
